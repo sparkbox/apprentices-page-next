fc81e40942507a4d9dd5c200415b418fc3177bf2
Alice Russell <alice@heysparkbox.com>
2021-10-19 08:35:35 -0500

    refactor: refactor Link tests
    References FSA21V2-132

--- components/link/link.test.js
+++ components/link/link.test.js
@@ -3,39 +3,18 @@
  */
 
 import * as React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Link from './link';
 
 describe('the Link component', () => {
-
-  test('renders a link given an href and text', () => {
-    render(
-      <Link href='http://some/url'>
-        hello!
-      </Link>
-    );
-
-    const link = screen.getByRole('link')
-    
-    expect(link).toHaveAttribute('href', 'http://some/url');
-    expect(
-      within(link).getByText('hello!')
-    ).toBeTruthy();
+  it('renders a link with the correct href given an href prop', () => {
+    render(<Link href='http://some/url'>hello!</Link>);
+    expect(screen.getByRole('link')).toHaveAttribute('href', 'http://some/url');
   });
 
-  test('renders a link given an href and text', () => {
-    render(
-      <Link href='http://some/url'>
-        <span role="button">goodbye</span>
-      </Link>
-    );
-  
-    const link = screen.getByRole('link');
-    const span = within(screen.getByRole('link')).getByRole('button');
-
-    expect(link).toHaveAttribute('href', 'http://some/url');
-    expect(span).toBeTruthy();
-    expect(within(span).getByText('goodbye')).toBeTruthy();
+  it('renders a link given an href and text', () => {
+    render(<Link href='http://some/url'>hello!</Link>);
+    expect(screen.getByRole('link')).toHaveTextContent('hello');
   });
 });
