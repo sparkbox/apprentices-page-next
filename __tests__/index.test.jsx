d2e5320629942491aea2037c56bba9de9b2648e4
Angus Chang <angus@heysparkbox.com>
2021-11-08 09:06:57 -0500

    test: new snapshot and heading on index
    - After adding our new components to the index page we
    needed to change the snapshot and also the text we check for the
    page heading
    
    FSA21V2-145

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,18 +2,18 @@
  * @jest-environment jsdom
  */
 
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />)
+    render(<Home />);
 
     const heading = screen.getByRole('heading', {
-      name: /Home/i,
-    })
+      name: /Apprenticeships at Sparkbox/i,
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
