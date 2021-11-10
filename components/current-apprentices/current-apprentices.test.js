372ba766599256133320f602a1ae2fadb112d0d2
Angus Chang <angus@heysparkbox.com>
2021-11-10 11:14:34 -0500

    test: create tests for current-apprentices
    - Write test to check order of current apprentices
    - Write test to check the version number is correct
    
    FSA21V2-148

---
+++ components/current-apprentices/current-apprentices.test.js
@@ -0,0 +1,54 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CurrentApprentices } from './current-apprentices.js';
+
+describe('the CurrentApprentices component', () => {
+  let renderedImages;
+  let version;
+
+  beforeAll(() => {
+    const currentApprenticeClass = {
+      version: '14.0',
+      currentApprentices: [
+        {
+          name: 'John Doe',
+          image: '/apprentices/John.png',
+          links: [
+            { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+            { href: 'personal', text: 'Personal' },
+            { href: 'https://www.github.com/', text: 'Github' },
+          ],
+        },
+        {
+          name: 'Jane Doe',
+          image: '/apprentices/Jane.png',
+          links: [
+            { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+            { href: 'personal', text: 'Personal' },
+            { href: 'https://www.github.com/', text: 'Github' },
+          ],
+        },
+      ],
+    };
+
+    render(<CurrentApprentices currentApprenticeClass={currentApprenticeClass} />);
+
+    renderedImages = screen.getAllByRole('img');
+    version = screen.getByRole('heading', {
+      name: /current apprentices/i,
+    });
+  });
+
+  it('renders the current apprentices in the correct order', () => {
+    expect(renderedImages[0]).toHaveAttribute('src', '/apprentices/John.png');
+    expect(renderedImages[1]).toHaveAttribute('src', '/apprentices/Jane.png');
+  });
+  it('renders the correct version number', () => {
+    expect(version).toHaveTextContent('v14.0');
+  });
+});
