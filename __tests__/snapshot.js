81b9be66354b27e49ff6277988e42c67a68fb993
Angus Chang <angus@heysparkbox.com>
2021-12-01 13:12:13 -0500

    test: add mock props for snapshot
    - mocked our props in our snapshot testing so our test
    does not rely on our actual data sets
    - updated our snapshot to render mock data
    
    FSA21V2-155

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -3,6 +3,47 @@ import renderer from 'react-test-renderer';
 import Index from '../pages/index';
 
 it('renders homepage unchanged', () => {
-  const tree = renderer.create(<Index />).toJSON();
+  const apprenticeData = {
+    currentApprenticeGroup: {
+      version: '0.0',
+      currentApprentices: [
+        {
+          name: 'First Last',
+          image: '/apprentices/First.png',
+          links: [
+            { href: 'personal', text: 'Personal' },
+            { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+            { href: 'https://www.github.com/', text: 'Github' },
+          ],
+        },
+        {
+          name: 'First Last',
+          image: '/apprentices/First.png',
+          links: [
+            { href: 'personal', text: 'Personal' },
+            { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+            { href: 'https://www.github.com/', text: 'Github' },
+          ],
+        },
+      ],
+    },
+    previousApprenticeGroups: [{
+      version: '0.0',
+      apprentices: [
+        {
+          name: 'First Last',
+        },
+        {
+          name: 'First Last',
+          status: 'previous',
+        },
+        {
+          name: 'First Last',
+          status: 'current',
+        },
+      ],
+    }],
+  };
+  const tree = renderer.create(<Index apprenticeData={apprenticeData} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
