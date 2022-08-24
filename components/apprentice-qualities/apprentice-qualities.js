a098a602337a3e56fb0badc7dd6f097d250e0c02
Catherine Meade <cat@heysparkbox.com>
2022-08-24 14:19:42 -0400

    feat: Copy in ApprenticeQualities includes three types of apprenticeships

--- components/apprentice-qualities/apprentice-qualities.js
+++ components/apprentice-qualities/apprentice-qualities.js
@@ -17,23 +17,34 @@ const ApprenticeQualities = () => (
         <div className={styles['apprentice-qualities__article-text']}>
           <p>
             We build responsive, standards-compliant, content-focused,
-            well-crafted, accessible, valuable, beautiful web sites and apps,
-            and we&apos;re looking for some web geeks that want to do the same.
+            well-crafted, accessible, valuable, beautiful websites and apps,
+            and we’re looking for some beginner web creators that want to do the same.
+
           </p>
+
           <p>
-            Passion, character, and initiative are a requirement.
-            A long resumé is not—as long as you care about the right things,
-            we can help build your skillset.
-            This is true for those we hire, and it is equally true for our apprentices.
-            Through your apprenticeship, we hope to help develop you into the sort of individual
-            we’d want to hire. And we may do just that.
+            Passion, character, and initiative are a requirement,
+            and we’re looking for those who have at least a beginning
+            knowledge of web development fundamentals. This apprenticeship
+            will help build your technical and professional skill set, as we
+            take a holistic approach to develop you into the sort of individual
+            we’d want to hire. And we may do just that!
+
           </p>
+
           <p>
-            We’re looking primarily for those interested in an apprenticeship
-            in web development—frontend, backend, or anything code related.
-            What we’re looking for most, however, are individuals who are
-            excited to make their mark on the web industry. In your application,
-            provide anything and everything you can that will convince us you are that geek.
+            We’re looking for folks interested in one of our three apprenticeship tracks:
+          </p>
+          <ul>
+            <li>Full-Stack Web Development</li>
+            <li>Frontend Design</li>
+            <li>UI/UX Design</li>
+          </ul>
+          <p>
+            What we’re looking for most, however, are individuals who
+            are excited to make their mark on the web industry. In your application,
+            provide anything and everything you can that will convince us
+            you are the best fit for this opportunity.
           </p>
         </div>
         <img
