fd5e208e5e8054c51572be60b0ff35f071e47fed
Osmond Arnesto <osmond@heysparkbox.com>
2022-09-13 13:30:14 -0700

    feat: Create LearnMore component

--- pages/apply/index.js
+++ pages/apply/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Head from 'next/head';
 import styles from './index.module.scss';
 import ApplyHero from '../../components/apply-hero/apply-hero';
+import LearnMore from '../../components/learn-more/learn-more';
 import SubscribeCTA from '../../components/subscribe-cta/subscribe-cta';
 import Footer from '../../components/footer/footer';
 
 const ApplyPage = () => (
   <div className={styles.container}>
     <Head>
-      <title>Become a Sparkbox Apprentice</title>
+      <title>Sparkbox Apprentices</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <ApplyHero />
 
     <main>
+      <LearnMore />
       <SubscribeCTA />
     </main>
 
